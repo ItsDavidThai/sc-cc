53f94b4fcef75051bbbc356395d209271b89bdac
ItsDavidThai <itsdavidthai@gmail.com>
2016-11-28 18:32:44 -0800

    add date to product item, add sort by name/date/price/asc/desc functionality

--- src/app/product-feed/product-feed.component.ts
+++ src/app/product-feed/product-feed.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiRequestsService } from '../services/api-requests.service'
+
 @Component({
   selector: 'app-product-feed',
   templateUrl: './product-feed.component.html',
@@ -7,6 +8,15 @@ import { ApiRequestsService } from '../services/api-requests.service'
 })
 export class ProductFeedComponent implements OnInit {
   private products: Array<Object>
+  private sortOptions = {
+    categories: {
+      Price: 'defaultPriceInCents',
+      Name: 'name',
+      Date: 'createdAt'
+    },
+    direction:'asc',
+    selectedCategory: 'defaultPriceInCents'
+  }
   constructor(private apiRequestsService: ApiRequestsService) { }
 
   ngOnInit() {
@@ -17,8 +27,29 @@ export class ProductFeedComponent implements OnInit {
     var that = this
     this.apiRequestsService.getJSONData().subscribe(function(result){
       that.products = result.products
-      console.log(that.products, 'another')
+      console.log(that.products)
     })
+  }
+  sortFeed(value){
+    var that = this
+    // passed in value can be undefined, if undefined set to valid input
+    value[1] = typeof value[1] === 'undefined' ? this.sortOptions.direction : value[1]
+    var direction = value[1] ? 'asc' : 'desc'
+    this.sortOptions.selectedCategory =  this.sortOptions.categories[value[0]] || this.sortOptions.selectedCategory
+    this.sortOptions.direction = direction
+
+    if(this.sortOptions.direction === 'asc') {
+      this.products.sort(function(a, b) {
+        return a[that.sortOptions.selectedCategory] > b[that.sortOptions.selectedCategory]
+      })
+    } else if ( this.sortOptions.direction === 'desc') {
+      this.products.sort(function(a, b) {
+        return a[that.sortOptions.selectedCategory] < b[that.sortOptions.selectedCategory]
+      })
+    }
+  }
+  filterFeed(){
 
   }
+
 }
--- src/app/product-filters/product-filters.component.ts
+++ src/app/product-filters/product-filters.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+// declare var $:any
 @Component({
   selector: 'app-product-filters',
   templateUrl: './product-filters.component.html',
   styleUrls: ['./product-filters.component.css']
 })
 export class ProductFiltersComponent implements OnInit {
+  @Output() sortChoosen: EventEmitter<Array> = new EventEmitter<Array>();
 
   constructor() { }
 
   ngOnInit() {
+    // $('select').material_select();
+  }
+
+  onChooseSort(sortOption) {
+    console.log(sortOption)
+    this.sortChoosen.emit([sortOption.target.value, sortOption.target.checked])
   }
 
 }
